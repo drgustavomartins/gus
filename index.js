9601107631086a68778fb2e204acda1fd1f82896
drgustavomartins <gustavo.m.martins@outlook.com>
2026-05-31 14:42:23 -0300

    Fix formatting of rateLimit import in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const axios = require("axios");
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+const rateLimit = require("express-rate-limit"); 
 const cors = require("cors");
 
 const app = express();
